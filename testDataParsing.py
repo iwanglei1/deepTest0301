c2bbecf49cffdf0ea27e54168712529b8f2c5d27
Lawther Wang <nixichuanqi@gmail.com>
2021-03-03 16:55:22 +0800

    readData successed

--- testDataParsing.py
+++ testDataParsing.py
@@ -4,19 +4,36 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.datasets import mnist
 import scipy.io as scio
-
-# X_data = np.random.rand(200)
-# # # print(X_data)
-# # # print(type(X_data))
-# # # print("数组的均值是：%d"%(np.mean(X_data)))
-# # # print("数组的方差是：%d"%(np.var(X_data)))
-# (train_images,train_labels),(test_images,test_lable) = mnist.load_data()
-# print(train_images.shape)
-# print(train_labels.shape)
-# for i in train_labels:
-#     # print(i)
-#     print(type(i))
-# for ii in train_images:
-#     print(ii.ndim)
-#     break
-# print(train_images.ndim)
+# 成功将需要的数据转换为矩阵
+dataFile = 'C://Users//Liangyi//Desktop//'
+dataName = 'corn.mat'
+data = scio.loadmat(dataFile+dataName)
+# for i in data:
+#     print(i)
+# print(data['m5spec'])
+# for m5 in data['m5spec']:
+#     print(m5)
+# print(data['m5spec'])
+# print(type(data['m5spec']))
+# print(data['m5spec'].data)
+datastr5 = data['m5spec']
+datastr5p = data['mp5spec']
+datastr6p = data['mp6spec']
+dataAll = data['propvals']
+# print(datastr5.dtype)
+# print(datast5r.shape)
+# print(datastr5.size)
+# print(datastr5[0,0]['data'])         #成功
+data5 = datastr5[0,0]['data']
+datap5 = datastr5p[0,0]['data']
+datap6 = datastr6p[0,0]['data']
+dataContAll = dataAll[0,0]['data']
+print("######################################################")
+print(data5)
+print("######################################################")
+print(datap5)
+print("######################################################")
+print(datap6)
+print("######################################################")
+print(dataContAll)
+print("######################################################")
