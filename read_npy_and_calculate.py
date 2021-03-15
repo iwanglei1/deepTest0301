807e9e5b54273da52e632b1a5fc92675d0203624
Lawther Wang <nixichuanqi@gmail.com>
2021-03-15 22:51:46 +0800

    Add save and read
    Signed-off-by: Lawther Wang <nixichuanqi@gmail.com>

---
+++ read_npy_and_calculate.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+shuujv = np.load('yuanshishujv.npy')
+print(shuujv)
--- save_npy.py
+++ save_npy.py
@@ -13,4 +13,7 @@ model = load_model('my_model.h5')
 chengfenshui,d5,dp5,dp6 = sF.getOdata()   #调用函数返回原始数据，数据已处理为均值为零，方差为1
 test_data,test_label = sF.getTestData(chengfenshui,d5,dp5,dp6)
 result = model.predict(test_data)
+np.save("yuanshishujv.npy",result)
 print(result)
+print(type(result))
+print(result.shape)
