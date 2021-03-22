2d5cfb41e774bc0d033eaf74e41fe00b154b63f0
Lawther Wang <nixichuanqi@gmail.com>
2021-03-22 20:03:34 +0800

    Add train_data RMSE
    Signed-off-by: Lawther Wang <nixichuanqi@gmail.com>

--- someFunction.py
+++ someFunction.py
@@ -80,14 +80,33 @@ def getTestData(chengfenshui,d5,dp5,dp6):
     return test_data,test_lable,train_data,train_lable
 ###########################################################################################################
 ## 计算RMSEC、RMSEP
-def calculate_RMSE(p_value,r_calue):
+def calculate_RMSE(p_value,r_value):
+
     cal_tem = 0
     cc_len = len(p_value)
     for i in range(cc_len):
-        tem = math.pow((p_value[i] - r_calue[i]),2)
+        tem = math.pow((p_value[i] - r_value[i]),2)
         cal_tem = cal_tem + tem
     cal_cc = cal_tem/cc_len
     cal_fin = math.sqrt(cal_cc)
     return cal_fin
 
 ##########################################################################################################
+## 计算决定系数R^2
+def calculate_R2(p_value,r_value):
+    average = r_value.mean(axis=0)
+    # print(average)
+    cr_len = len(r_value)
+    car_tem = 0
+    cars_tem = 0
+    for i in range(cr_len):
+        temp_r = math.pow(r_value[i]-average,2)
+        car_tem = car_tem + temp_r
+    for i2 in range(cr_len):
+        temp_s = math.pow(r_value[i2]-p_value[i2],2)
+        cars_tem = cars_tem + temp_s
+    r_2 = 1-(cars_tem/car_tem)
+    print(car_tem)
+    print(cars_tem)
+    return r_2
+#########################################################################################################
