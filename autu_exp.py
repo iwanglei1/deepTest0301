5e1bde6c2fc95b8f85f56bf65c3f7103024013f9
Lawther Wang <nixichuanqi@gmail.com>
2021-03-28 20:58:30 +0800

    [MOD] 提高到0.136
    Signed-off-by: Lawther Wang <nixichuanqi@gmail.com>

---
+++ autu_exp.py
@@ -0,0 +1,10 @@
+import regulating_Hyperparameter as aU
+import numpy as np
+
+lunshu = 50
+rec_rmsep = np.empty(lunshu,dtype=float)
+for i in range(lunshu):
+   rec_rmsep[i] = aU.au_Exp()
+
+rec_min = np.min(rec_rmsep)
+print('训练了%d轮，最小的RMSEP是：%f'%(lunshu,rec_min))
