0aab5e543bd75176ef463af1517d72ef776ae5ad
Lawther Wang <nixichuanqi@gmail.com>
2021-03-29 20:36:28 +0800

    [ADD] RMSEP dp5 油
    Signed-off-by: Lawther Wang <nixichuanqi@gmail.com>

--- FFFFFF.py
+++ FFFFFF.py
@@ -8,7 +8,7 @@ from openpyxl import load_workbook
 # print(type(now.strftime("%Y-%m-%d %H:%M:%S")))
 
 
-result2=[('d5','油','#####','#####','####','####','####')]#需要新写入的数据
+result2=[('dp6','油','#####','#####','####','####','####')]#需要新写入的数据
 # result2=[(now_s,epochs_au,batch_size_au,rmsec,r_2_t,rmsep,r_2_p)]#需要新写入的数据
 df = pd.DataFrame(result2,columns=['model_name','epochs','batch_size','RMSEC','R_C','RMSEP','R_P'])#列表数据转为数据框
 df.to_csv('shiyanshujv.csv',mode='a',index=False,header=False)
